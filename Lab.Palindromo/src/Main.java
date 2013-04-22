e64aa842d0fc1010c483735107b756422264c311
Mario Jorge Pereira <mariojp@gmail.com>
2013-04-22 17:11:43 -0300

    Desafio da Aula 1

---
+++ Lab.Palindromo/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String palavra = "ABBA";
+		char[] array = palavra.toCharArray();
+		boolean palindromo = true;
+		for (int i = 0, j = array.length-1; i < (array.length / 2); i++, j--) {
+			if (array[i] != array[j]) {
+				palindromo = false;
+			}
+		}
+
+		if (palindromo) {
+			System.out.println("A palavra " + palavra + " é um palíndromo.");
+		} else {
+			System.out
+					.println("A palavra " + palavra + " não é um palíndromo.");
+		}
+	}
+
+}
