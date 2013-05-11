ad37cfff4b2b12e55bb69d35778c7073639fd384
Mario Jorge Pereira <mariojp@gmail.com>
2013-05-11 12:13:39 -0300

    Laboratorio Servlet

---
+++ LabServletsI/src/br/com/parallel/Relogio.java
@@ -0,0 +1,47 @@
+package br.com.parallel;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Relogio
+ */
+public class Relogio extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public Relogio() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+		HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		java.util.Date agora = new java.util.Date();
+		out.println("<html>" + "<body>"
+				+ "<h1 align=center>Servlet versão 2.5</h1>" + "<br />" + agora
+				+ "</body>" + "</html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		this.doGet(request, response);
+	}
+
+}
